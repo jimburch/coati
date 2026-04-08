dc7d381de5c87d2bb485e260301eef23ecef9f04
Jim Burch <49823725+jimburch@users.noreply.github.com>
2026-04-07 22:27:48 -0700

    fix: ci/cd improvements for coolify (#234)

--- release.config.js
+++ release.config.js
@@ -8,7 +8,7 @@ export default {
 		[
 			'@semantic-release/git',
 			{
-				assets: ['CHANGELOG.md', 'package.json'],
+				assets: ['CHANGELOG.md'],
 				message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
 			}
 		],
